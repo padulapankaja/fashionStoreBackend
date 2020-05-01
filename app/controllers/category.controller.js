2355349fbee70a6fb683f2bcab50636fcc79e16d
amoda-sasmitha <amoda29@gmail.com>
2020-05-01 23:18:16 +0530

    Categories crud added

---
+++ app/controllers/category.controller.js
@@ -0,0 +1,107 @@
+const Category = require('../models/category.model');
+
+exports.insert = (req, res ,next ) => {
+    
+    let new_category = Category({
+        name: req.body.name,
+        banner_title: req.body.banner_title,
+        banne_subtitle: req.body.banne_subtitle,
+        banner_image: req.file.path,
+        created_at: new Date() ,
+        updated_at: new Date()
+    });
+
+    new_category.save( (err ,result ) => {
+        if (err) { return next(err)}
+        
+        data = {
+            status : 'success',
+            code : 200,
+            data : result,
+            message : 'Category Added Successfully'
+        }
+        
+        res.json(data)
+    })
+}
+
+exports.getAll = (req, res ,next ) => {
+    
+    Category.find({} , (err, result) => {
+        if(err){ return next(err) }
+
+        data = {
+            status : 'success',
+            code : 200,
+            data : result
+        }
+    
+        res.json(data);
+    });
+}
+
+exports.update = (req, res ,next ) => {
+    const id = req.params.id
+    Category.findOne({ _id : id } , (err, found_category ) => {
+        if(err){ return next(err) }
+
+        //if object not found
+        if(!found_category){
+            res.status(404).send();
+        }else{
+
+            //if name changed
+            if( req.body.name){ found_category.name = req.body.name }
+
+            //if title changed
+            if( req.body.banner_title){ found_category.banner_title = req.body.banner_title }
+
+            //if subtitle changed
+            if( req.body.banne_subtitle){ found_category.banne_subtitle = req.body.banne_subtitle }
+
+            //if image changed
+            if(req.file ){ found_category.banner_image = req.file.path }
+
+            found_category.updated_at = new Date();
+
+            found_category.save( (err , updated_object) => {
+                if(err){ return next(err) }
+                
+                data = {
+                    status : 'success',
+                    code : 200,
+                    data : updated_object,
+                    message : 'Successfully Updated'
+                }
+                res.json(data);
+
+            })
+
+        }
+    });
+}
+
+exports.delete = (req,res,next) => {
+    const id = req.params.id
+    Category.findOne({ _id : id } , (err, found_category ) => {
+        if(err){ return next(err) }
+
+        //if object not found
+        if(!found_category){
+            res.status(404).send();
+        }else{
+            found_category.remove( (err, result) => {
+                if(err){ return next(err) }
+
+                data = {
+                    status : 'success',
+                    code : 200,
+                    data : result,
+                    message : 'Successfully Removed'
+                }
+                res.json(data);
+
+            })
+        }
+    }) 
+}
---
+++ app/routes/category.router.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const cateogry = require('../controllers/category.controller');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, res, cb) {
+        cb(null, './uploads/categories/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().getTime().toString() + "_" + file.originalname);
+    }
+})
+
+const fileFiler = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        // accept
+        cb(null, true)
+    } else {
+        // reject
+        cb(new Error('message : file not acceptable'), false)
+    }
+}
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 5 },
+    fileFilter: fileFiler
+});
+
+
+//--------------------------------------------------------------------
+
+//insert new category
+router.post('/insert' , upload.single('photos') ,  cateogry.insert );
+
+//get all categories
+router.get('/getall' , cateogry.getAll );
+
+//update categary by id
+router.patch('/update/:id' , upload.single('photos') , cateogry.update );
+
+//delete category by id
+router.delete('/delete/:id' , cateogry.delete );
+
+module.exports = router;
