68352444dc70759b3bba7680e4a67dfe09abf43d
ozandabb <osanda.gamage.79@gmail.com>
2020-05-18 09:13:49 +0530

    wishlist code change

--- app/controllers/wishlist.controller.js
+++ app/controllers/wishlist.controller.js
@@ -4,16 +4,7 @@ exports.Insert = (req, res, next) => {
   let newWishlistItem = wishlist({
     userid: req.body.userid,
     product_id: req.body.product_id,
-    pruduct_name: req.body.pruduct_name,
-    product_price: req.body.product_price,
-    description: req.body.description,
-    size: req.body.size,
-    color: req.body.color,
-    image: req.body.image,
-    is_active: req.body.is_active,
-    discount: req.body.discount,
-    view_count: req.body.view_count,
-    created_at: req.body.created_at,
+    created_at: new Date(),
   });
 
   console.log("New WishList Item: ", newWishlistItem);
@@ -27,7 +18,7 @@ exports.Insert = (req, res, next) => {
       status: "success",
       code: 200,
       data: result,
-      message: "Order Added Successfully",
+      message: "Item Added Successfully",
     };
 
     res.json(data);
@@ -35,19 +26,41 @@ exports.Insert = (req, res, next) => {
 };
 
 exports.GetAll = (req, res, next) => {
-  wishlist.find({}, (err, result) => {
-    if (err) {
-      return next(err);
-    }
+  let userid = req.params.userid;
+  console.log(userid);
 
-    data = {
-      status: "success",
-      code: 200,
-      data: result,
-    };
+  wishlist
+    .aggregate([
+      { $match: { users: ObjectId(userid) } },
+      {
+        $lookup: {
+          from: "products", // collection name in db
+          localField: "product_id",
+          foreignField: "_id",
+          as: "product",
+        },
+      },
+      {
+        $project: {
+          userid: 1,
+          created_at: 0,
+          product: { $arrayElemAt: ["$product", 0] },
+        },
+      },
+    ])
+    .exec(function (err, result) {
+      if (err) {
+        return next(err);
+      }
 
-    res.json(data);
-  });
+      data = {
+        status: "success",
+        code: 200,
+        data: result,
+      };
+
+      res.json(data);
+    });
 };
 
 exports.Delete = (req, res, next) => {
--- app/models/comment.model.js
+++ app/models/comment.model.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 let Comment = new Schema({
   userid: {
     type: String,
-    required: true,
   },
   username: {
     type: String,
