069e8a80a1101179e543ebd430a0abf7e169699d
amoda-sasmitha <amoda29@gmail.com>
2020-05-23 14:14:49 +0530

    dashboard functions added

--- app/controllers/common.controller.js
+++ app/controllers/common.controller.js
@@ -1,6 +1,6 @@
 //import User model
 const NewsLetter = require('../models/newsletters.model');
-
+const moment = require('moment')
 const UtilObj = require('../util/util')
 
 const Admin = require('../models/admin.model');
@@ -10,6 +10,7 @@ const Products = require('../models/product.model');
 const Categories = require('../models/category.model');
 const Orders = require('../models/order.model');
 const Offers = require('../models/offer.model');
+const Comments = require('../models/comment.model')
 
 //======================================================================================================
 //===================================  ADD SUBSCRIBE      ==============================================
@@ -89,7 +90,78 @@ exports.counts = async function (req, res, next) {
 
     res.json(data);
    
+}
 
+exports.latest_comments = function (req, res, next) {
+   
+    Comments.aggregate([
+        { $sort : { created_at : -1 } },
+        { $limit : parseInt(req.params.limit) }
+    ])
+    .exec(function(err, result) {
+        if (err) { return next(err)}
+   
+    data = {
+        status : 'success',
+        code : 200,
+        data : result
+    }
+
+    res.json(data);
+    });
+   
+}
+
+exports.revenue = function (req, res, next) {
+    
+    var today = new Date(moment( new Date() ).format("YYYY-MM-DD") ); 
+    var prev =  new Date(today - 12096e5 );
+    let dataset = []
+    
+    Orders.aggregate([
+        {
+            $project: {
+                  xx : { $toDouble : "$amount" },
+                  orderdate : { $dateToString: { format: "%Y-%m-%d", date: "$date" } }
+            }
+        },
+        {
+            $group : { _id : "$orderdate"  , total  : { $sum :  '$xx'}  }
+        }
+    ]).exec(function(err, result) {
+        if (err) { return next(err)}
+        
+
+        let revenue = result.map( item => {
+            return{
+                date : new Date(item._id),
+                total : item.total
+            }
+        })
+
+        for(var arr=[],dt= prev ; dt<= today; dt.setDate(dt.getDate()+1)){ 
+            
+            let c = new Date(dt);
+            let total = 0;
+            let find = revenue.findIndex( item => item.date.getTime() == c.getTime() )
+            if(find >= 0 ){
+                total = revenue[find].total;
+            }
+
+            dataset.push({
+               date : c,
+               total : total   
+            })
+
+          }
+        
+          data = {
+            status : 'success',
+            code : 200,
+            data : dataset,
+        }
+        res.json(data)
+    });
 
 }
 
--- app/routes/common.route.js
+++ app/routes/common.route.js
@@ -7,6 +7,7 @@ router.post("/subs", Common.subscribe);
 router.post("/forget", Common.forgotPassword);
 
 router.get("/counts" , Common.counts )
-
+router.get("/revenue" , Common.revenue )
+router.get("/latestcomments/:limit" , Common.latest_comments )
 
 module.exports = router;
